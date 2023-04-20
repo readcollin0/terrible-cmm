4dc40947eab65d0265aa5bde922761d246426b1c
Collin Read <readcollin0@gmail.com>
2023-04-19 22:44:02 -0600

    Clean up a little more

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbcl --non-interactive --load parser.lsp --eval "(full-compile \"$1\" \"$2\" :preproc-out \"$2.pre\" :init-stack 16384 :trim-functions t)"
+sbcl --non-interactive --load parser.lsp --eval "(full-compile \"$1\" \"$2\" :init-stack 16384 :trim-functions t)"
